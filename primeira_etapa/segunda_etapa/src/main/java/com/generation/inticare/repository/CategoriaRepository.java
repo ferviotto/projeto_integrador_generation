95c1f75b4be26c74d4a99cea05bb98cffb7bae9a
Fernanda Viotto De Gobbi <164960590+ferviotto@users.noreply.github.com>
2024-07-05 18:14:33 -0300

    Delete primeira_etapa directory

--- primeira_etapa/segunda_etapa/src/main/java/com/generation/inticare/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.generation.inticare.repository;
-
-import com.generation.inticare.model.CategoriaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
-}
