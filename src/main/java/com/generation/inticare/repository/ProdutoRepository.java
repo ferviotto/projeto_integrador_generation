6e890e1fc88f90b95c5fb5fda5ae17ea184ad75b
Fernanda_Viotto_De_Gobbi <fernandavdgobbi@gmail.com>
2024-07-12 19:46:30 -0300

    Quarta Etapa

---
+++ src/main/java/com/generation/inticare/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.generation.inticare.repository;
+
+import com.generation.inticare.model.ProdutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>  {
+	List<ProdutoModel> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
+
+}
